9b16840ae29eff1d5d0b6a4a4b0cbad803d28039
aurel <aurelianfrangin@gmail.com>
2025-09-30 20:28:59 +0200

    removal of old tests in main

--- MNIST_neural_network/MNIST_neural_network.cpp
+++ MNIST_neural_network/MNIST_neural_network.cpp
@@ -40,52 +40,18 @@ int main()
 	printMnistDigit(reader.training_images[0].data(), 28);
 	cout << "Label: " << int(reader.training_labels[0]) << endl;
 
-	// --- test matrix multiplication ---
-
-	/*GLuint ssboResult;
-	glGenBuffers(1, &ssboResult);
-
-    GLdouble *mat1 = new GLdouble[common_length * height_left];
-    GLdouble *mat2 = new GLdouble[width_right * common_length];
-    fillMatrix<GLdouble>(mat1, common_length, height_left);
-    fillMatrix<GLdouble>(mat2, width_right, common_length);
-
-    printMatrix<GLdouble>(mat1, common_length, height_left);
-    std::cout << std::endl;
-    printMatrix<GLdouble>(mat2, width_right, common_length);
-    std::cout << std::endl;
-  
-
-    matrix_add<GLdouble>(mat1, mat1, ssboResult, common_length, height_left);
-	glBindBuffer(GL_SHADER_STORAGE_BUFFER, ssboResult);
-	GLdouble* result = (GLdouble*)glMapBuffer(GL_SHADER_STORAGE_BUFFER, GL_READ_ONLY);
-
-	cout << "Result:" << endl;
-    printMatrix<GLdouble>(result, common_length, height_left);
-    cout << endl;
-
-    glDeleteBuffers(1, &ssboResult);
-    delete[] mat1;
-    delete[] mat2;*/
 
 
 	// --- test neural network ---
 	
-	const int inputSize = 15;
+    const unsigned int inputSize = 15;
+    const unsigned int inputNumber = 2;
 
-    float input_img[inputSize] = {
-        0.1, 0.2, 0.3, 0.4, 0.5,
-        0.6, 0.7, 0.8, 0.9, 0.85,
-        0.6, 0.7, 0.8, 0.3, 0.4
+    float input_img[inputNumber *inputSize] = {
+        0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.85, 0.6, 0.7, 0.8, 0.3, 0.4,
+        0.8, 0.9, 0.85, 0.6, 0.7, 0.8, 0.3, 0.2, 0.3, 0.4, 0.5, 0.6, 0.8, 0.3, 0.4
 	};
-	float input_vec[3] = { 0.1, 0.2, 0.3 };
-	GLuint ssboResult;
-	glGenBuffers(1, &ssboResult);
-	matrix_add_constant_vec<float>(input_img, input_vec, ssboResult, inputSize/3, 3);
-	glBindBuffer(GL_SHADER_STORAGE_BUFFER, ssboResult);
-	float* result = (float*)glMapBuffer(GL_SHADER_STORAGE_BUFFER, GL_READ_ONLY);
-	printMatrix<float>(result, inputSize/3, 3);
-
+	
     // for each layer, number of columns (width) must match size of input (number of pixels for first layer / previous number of neurons)
 	// and number of rows (height) is the number of neurons in the layer
     vector<unsigned int> neuronsPerLayer({ 10, 3 });
